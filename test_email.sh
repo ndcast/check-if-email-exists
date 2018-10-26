9621abf0f300ed5be53deaf25ca0efa91db03b2b
ndcast <ndcastt@gmail.com>
2018-10-26 02:08:05 -0600

    Refined grep in OUTPUT variable
    When you try for hosts with MX recprd but are not mailservers, there's a disconnect exception from telnet.
    Added ' grep -v "closed"  ' to the conditional for $OUTPUT to avoid having a false positive.
    
    Try with anymail@hotd.com to check this fact.

---
+++ test_email.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# Set useful variables
+EMAIL="$1";
+DOMAIN=`cut -d "@" -f 2 <<< "$1"`;
+MX_HOSTS=(`nslookup -q=mx $DOMAIN | grep mail | cut -d " " -f 5`) # Find MX hosts of our domain
+PORTS=(25 587 465); # The PORTS to test our telnet on, in this order
+
+if [[ $# == 2 ]]; then
+    SENDER="$2"
+fi
+if [ -z $SENDER ]; then
+    SENDER='email@example.com'
+fi
+
+
+  for HOST in ${MX_HOSTS[@]}; do
+      for PORT in ${PORTS[@]}; do
+
+        #echo "$HOST : $PORT "
+
+          OUTPUT=`expect expectTelnet.tcl $EMAIL $HOST $PORT $SENDER 2>/dev/null`;
+          if [[ `echo $OUTPUT | grep "2.1.5"| grep -v "closed" ` ]]; then # 2.1.5 means address exists
+              # echo $OUTPUT;
+                echo true;
+              exit 0;
+          else
+                echo false;
+           exit 1;
+        fi
+      done
+  done
+
+echo false;
+exit 1;
